634a441be3afe15dc2f7e7a7441125894a097e27
Ahmed-Al-B <ahmedalbadri002@gmail.com>
2024-01-29 18:39:52 -0800

    Hw One

---
+++ hw1/01-hello.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', loadPage);
+
+function loadPage(event) {
+  console.log('The page has now loaded.');
+  console.log('My name is Ahmed Al-Badri and am a student in this class term which is Winter 2024, this term might be a good term becasue of the topics being taught in other classes and this class. However, during the choicing of the terms classes, there seemed to be many classes to take, and since this is the third year, more Computer Science classes became open.')
+}
